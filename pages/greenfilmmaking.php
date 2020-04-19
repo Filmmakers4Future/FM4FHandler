cf1cb4a422939d2b900b1f8f65b6d5c56e261ff5
Paul-Vincent Roll <paviro@me.com>
2020-04-20 00:54:54 +0200

    Added Green Shooting Pass and Green Film info

--- pages/greenfilmmaking.php
+++ pages/greenfilmmaking.php
@@ -27,6 +27,7 @@
         </ul>
       </div>
     </section>
+    
     <section class="page-section bg-primary text-right">
       <div class="container">
         <h3 class="text-white-50">Get rid of plastic bottles, disposable cups and dishes</h3>
@@ -35,6 +36,7 @@
         <p class="text-white">A possible solution can be giving ever crew member a <b>durable cup or bottle</b> they can <b>clip to their belt</b> and <b>reuse</b> for the whole production. Even better ask your crew beforehand if they need one or <b>might already have one</b> from a previous production. Provide <b>tap water</b> in areas where it is available and safe to drink. If you provide bottled water, choose <b>reusable glass bottles</b> and make them easily available so people can fill their own cups and bottles with them. Source water <b>filled locally</b> that does not have to be transported very far.</p>
       </div>
     </section>
+    
     <section class="page-section bg-dark text-left">
       <div class="container">
         <h3 class="text-white-50 mt-0">Waste separation</h3>
@@ -42,6 +44,7 @@
         <p class="text-white">Even more importantly: Make your crew aware of how to avoid garbage and support them in the process.</p>
       </div>
     </section>
+    
     <section class="page-section bg-primary text-right">
       <div class="container">
         <h3 class="text-white-50 mt-0">Make use of organic regional and seasonal foods - reduce meat</h3>
@@ -49,6 +52,7 @@
         <p class="text-white mt-0">Way too often the vegetarian and vegan menu feel like a second-class option. It's the same dish, but the meat has been replaced by tofu, for instance. Meatless dishes can be very nutritious, healthy and incredibly delicious. If the vegetarian dish is regarded as an independent and first-class option by the catering provider, one quickly notices that even non-vegetarians increasingly choose it, even if meat is offered as well. Once you have arrived at this point, you can completely omit meat on individual days without any major complaints. Especially if you simultaneously educate your crew about the reasons behind this decision.</p>
       </div>
     </section>
+    
     <section class="page-section bg-dark text-left">
       <div class="container">
         <h3 class="text-white-50 mt-0">Eliminate idling of cars and generators</h3>
@@ -59,12 +63,14 @@
         </ul>
       </div>
     </section>
+    
     <section class="page-section bg-primary text-right">
       <div class="container">
         <h3 class="text-white-50 mt-0">Educate your crew and team members - inspire them to talk about it</h3>
         <p class="text-white mt-0">When it comes to this issue it is vital that everyone is on board. Start conversations about how to be greener, incorporate the topic into your daily safety meetings and help people realize the importance of treating our planet better. It does not help if you are fully aware of this but your team is not. So inspire them to come up with their own ideas and when they do support them. Ideally, appoint a responsible person to whom people can turn in matters concerning climate protection. The goal must be that every member of your team thinks about the ecological consequences of their actions and knows how important it is to do the right thing.</p>
       </div>
     </section>
+    
     <section class="page-section bg-dark text-left">
       <div class="container">
         <h3 class="text-white-50 mt-0">Improve your office</h3>
@@ -81,30 +87,56 @@
         <p class="text-white mt-0">Then check which purchases you can make in a sustainable way - used, fair trade, environmentally friendly. Which work processes can be optimized with regard to their resource and financial efficiency (e.g. printing on both sides of recycled paper) on the one hand and with regard to employee satisfaction (e.g. ergonomics, exercise, catering) on the other.</p>
       </div>
     </section>
-    <!--
-    <section class="page-section bg-primary text-right">
-      <div class="container">
-        <h3 class="text-white-50 mt-0">8. Lorem ipsum dolor sit amet</h3>
-        <p class="text-white mt-0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-      </div>
-    </section>
-    <section class="page-section bg-dark text-left">
-      <div class="container">
-        <h3 class="text-white-50 mt-0">9. Lorem ipsum dolor sit amet</h3>
-        <p class="text-white mt-0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-      </div>
-    </section>
-    <section class="page-section bg-primary text-right">
-      <div class="container">
-        <h3 class="text-white-50 mt-0">10. Lorem ipsum dolor sit amet</h3>
-        <p class="text-white mt-0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-      </div>
-    </section>
-    -->
+  
     <section class="page-section bg-primary text-right">
       <div class="container">
         <h3 class="text-white-50 mt-0">Don't stop here!</h3>
         <p class="text-white mt-0"><b>To be clear:</b> Doing the 10 things mentioned on this site can be a good start but they are nowhere near enough! In order for your film or show to be ecologically sustainable it is vital to calculate it's unique footprint before the production begins and then develop a plan on how exactly this footprint can be minimized.</p>
+      </div>
+    </section>
+    
+    <section class="page-section bg-dark text-center">
+      <div class="container">
+        <h2 class="text-center text-white mt-0 mb-5">Further resources</h2>
+         <div class="row justify-content-center">
+        
+            <!-- Green Shooting Card -->
+            <div class="col-xl-4 col-lg-5 col-md-10 col-sm-10 col-10 mx-5 mb-3">
+              <figure class="figure">
+                <a target="_blank" rel="noopener noreferrer" href="https://www.ffhsh.de/de/film_commission/gruener_drehpass.php"><img src="/files/logos/Gruener_Drehpass.svg" width="160px" class="figure-img img-fluid mb-3" alt="Green Shooting Card Logo"></a>
+                <figcaption class="text-center text-white">Green Shooting Initiative of the Hamburg Film Fund in Germany.</figcaption>
+                <figcaption class="text-left text-white">
+                <ul>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://www.ffhsh.de/download/97_Sonstiges/GruenerDrehpass_Flyer_edition2.pdf.pdf">Leaflet</a> <span class="text-center text-white-50 small">[English + German]</span></li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://www.ffhsh.de/download/Best_Practice_Guide_D_2016_03.pdf">Best Practice Guide</a> <span class="text-center text-white-50 small">[German]</span></li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://www.ffhsh.de/download/GreenProductionGuide_30102015.pdf">Green Service Providers</a> <span class="text-center text-white-50 small">[German]</span></li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://www.ffhsh.de/download/GruenerDrehpass_Checkliste.pdf">Checklist</a> <span class="text-center text-white-50 small">[German]</span></li>
+                </ul>
+                </figcaption>
+              </figure>
+            </div>
+            <!-- End Green Shooting Card -->
+            
+            <!-- Green Film -->
+            <div class="col-xl-4 col-lg-5 col-md-10 col-sm-10 col-10 mx-5 mb-3">
+              <figure class="figure">
+                <a target="_blank" rel="noopener noreferrer" href="https://www.green.film"><img src="/files/logos/green-film-logo.svg" width="200px" class="figure-img img-fluid mb-3" alt="Green Film Logo"></a>
+                <figcaption class="text-center text-white">Italian rating system for sustainable film production.</figcaption>
+                <figcaption class="text-left text-white">
+                <ul>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://www.green.film/how-does-it-work.php">How does it work?</a> <span class="text-center text-white-50 small">[English]</span></li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://www.green.film/criteria-green-film.php">Evaluation Criteria</a> <span class="text-center text-white-50 small">[English]</span></li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://www.green.film/why-adopt-green-film.php">Why adopt it?</a> <span class="text-center text-white-50 small">[English]</span></li>
+                <li><a target="_blank" rel="noopener noreferrer" href="https://www.green.film/Rating-system-green-film-ENG.pdf">Rating System</a> <span class="text-center text-white-50 small">[English]</span></li>
+                </ul>
+                </figcaption>
+              </figure>
+            </div>
+            <!-- End Green Film -->
+            
+            
+            
+         </div>
       </div>
     </section>
 <?php
